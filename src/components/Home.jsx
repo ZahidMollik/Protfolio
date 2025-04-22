97d97002a65555530d4937aa30482ce1ecb969e3
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2025-04-22 23:59:44 +0600

    added home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-screen'>Home</div>
+    <div className='w-screen h-screen flex justify-evenly items-center font-sans'>
+      <div className='flex flex-col gap-36'>
+        <div className='w-[390px]'>
+          <h1 className='text-4xl font-semibold pb-3'>Hello, I'm</h1>
+          <h1 className='text-4xl font-semibold pb-3'>Md. Zahidul Islam Mollik</h1>
+          <span className='text-xs text-gray-500'>I'm a Blockchain Developer based in Dhaka, Bangladesh, focused on building user-centric solutions across smart contracts, NFTs, and
+          DeFi platforms.</span>
+          <div className='mt-3 px-5 py-3 w-30 bg-cyan-500 rounded-md text-white font-semibold hover:bg-teal-500'>Say Hello!</div>
+        </div>
+        <div className='flex gap-1'>
+          <div className='px-5 py-3 w-38 bg-cyan-100 text-center'>
+            <h2 className='font-bold text-lg'>1 Y.</h2>
+            <span className='text-xs text-gray-400'>Experience</span>
+          </div>
+          <div className='px-5 py-3 w-38 bg-cyan-100  text-center'>
+            <h2 className='font-bold text-lg'>25+</h2>
+            <span className='text-xs text-gray-400'>Project Completed</span>
+          </div>
+          <div className='px-5 py-3 w-38 bg-cyan-100  text-center'>
+            <h2 className='font-bold text-lg'>19</h2>
+            <span className='text-xs text-gray-400'>Happy Client</span>
+          </div>
+        </div>
+      </div>
+      <div >
+        <img className='w-[350px] h-[500px] rounded-2xl object-cover' src="src/assets/images/zahid.jpg" alt="" />
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className='w-full h-20 flex justify-around items-center shadow'>
       <div className='logo font-sans font-extrabold text-2xl'>
         <Link to='/' className='flex items-center'>
-          <h1 className=' w-10 h-10 rounded-full bg-teal-500 text-white flex items-center justify-center'>Z</h1>
+          <h1 className='px-3.5 py-1.5 rounded-full bg-cyan-500 text-white hover:bg-teal-500'>Z</h1>
           <h1 >ahid</h1>
         </Link>
       </div>
@@ -23,13 +23,13 @@ const Navbar = () => {
           <li>Process</li>
         </Link>
         <Link className='hover:text-cyan-500'  to="/project">
-          <li className='p-5 w-14'>Project</li>
+          <li>Project</li>
         </Link>
         <Link className='hover:text-cyan-500' to="/services">
-          <li className='p-5 w-14'>Services</li>
+          <li>Services</li>
         </Link>
         <Link  to="/contact">
-          <li className='w-20 h-8 bg-teal-500 text-center rounded-md text-white flex items-center justify-center hover:bg-cyan-500'>Contact</li>
+          <li className='px-5 py-2 bg-cyan-500 rounded-md text-white hover:bg-teal-500'>Contact</li>
         </Link>
       </ul>
 
